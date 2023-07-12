bce7c543585f54e4928b7958424ad7d5e26cd457
Ahmad Chalabi <ahmadchalabi0o@gmail.com>
2023-07-12 14:07:56 -0400

    authBranch

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(session({
+    secret: 'theSecret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 360000 }
+  }))
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const config = new DocumentBuilder()
     .setTitle('Median')
